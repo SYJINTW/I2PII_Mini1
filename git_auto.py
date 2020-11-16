e125ad8c1496b0b1c8d9e08d0cad3cb7e1c7a51b
SYJINTW <yuanjiun911@outlook.com>
2020-11-16 08:48:36 +0800

    test push commit

--- git_auto.py
+++ git_auto.py
@@ -4,7 +4,7 @@ commit_flag = int(input("Commit or not? (1/0): "))
 if commit_flag == 1:
 	commit_cmd = input("Commit_cmd: ")
 	os.system('git commit -m "' + commit_cmd + '"')
+	push_flag = int(input("Push or not? (1/0): "))
 	if push_flag == 1:
-		push_flag = int(input("Push or not? (1/0): "))
 		os.system("git push -u origin master")
 print("Finish")
