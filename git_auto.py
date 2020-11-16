1aa7f41da59ac950745fea569dd4f8d3b1823d74
SYJINTW <yuanjiun911@outlook.com>
2020-11-16 08:56:02 +0800

    Finish auto_git

--- git_auto.py
+++ git_auto.py
@@ -1,14 +1,17 @@
 import os
-add_flag = int(input("Add or not? (1/0): "))
-commit_flag = int(input("Commit or not? (1/0): "))
-commit_cmd = input("Commit_cmd: ")
-push_flag = int(input("Push or not? (1/0): "))
 
+add_flag = int(input("Add or not? (1/0): "))
 if add_flag == 1:
 	os.system("git add .")
+
+commit_flag = int(input("Commit or not? (1/0): "))
 if commit_flag == 1:
+	commit_cmd = input("Commit_cmd: ")
 	os.system('git commit -m "' + commit_cmd + '"')	
+
+push_flag = int(input("Push or not? (1/0): "))
 if push_flag == 1:
 		os.system("git push -u origin master")
+
+print("\n---- STATUS ----\n")
 os.system("git status")
-print("Finish")
