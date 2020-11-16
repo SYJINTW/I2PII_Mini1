51a6257399bc4e78beed414a29f5d51f553f1d9e
SYJINTW <yuanjiun911@outlook.com>
2020-11-16 08:48:09 +0800

    test auto commit

--- git_auto.py
+++ git_auto.py
@@ -1,6 +1,10 @@
 import os
-commit_cmd = input("Commit_cmd: ")
 os.system("git add .")
-os.system('git commit -m "' + commit_cmd + '"')
-os.system("git push -u origin master")
+commit_flag = int(input("Commit or not? (1/0): "))
+if commit_flag == 1:
+	commit_cmd = input("Commit_cmd: ")
+	os.system('git commit -m "' + commit_cmd + '"')
+	if push_flag == 1:
+		push_flag = int(input("Push or not? (1/0): "))
+		os.system("git push -u origin master")
 print("Finish")
